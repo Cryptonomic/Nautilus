9a4bd4836eb5b30f6e8c9b1801d7be6adf80b36e
leenpaws <s.chhaya@gmail.com>
2019-02-26 10:15:00 -0500

    Changes to be committed: 	deleted:    docker/app/conseil/env/local/conseil.conf 	deleted:    docker/app/conseil/env/local/runconseil-lorre.sh 	deleted:    docker/app/parse_opt.sh 	deleted:    docker/app/postgres/build.sh 	deleted:    docker/app/tezos/build.sh

--- docker/app/conseil/env/local/runconseil-lorre.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-java -Xms512m -Xmx14g -Dconfig.file=conseil.conf -cp conseil.jar tech.cryptonomic.conseil.Lorre alphanet &
-
-java -Xms512m -Xmx2g -Dconfig.file=conseil.conf -cp conseil.jar tech.cryptonomic.conseil.Conseil
