6de17dab1720a237f176ac344fd570a90b4fd958
leenpaws <s.chhaya@gmail.com>
2019-02-26 08:55:12 -0500

    modified:   docker/nautilus.sh

--- docker/nautilus.sh
+++ docker/nautilus.sh
@@ -126,10 +126,10 @@ build_conseil () {
     line1=`echo $line1`
     line2=`echo $line2`
     line3=`echo $line3`
-    conseil_conf_file=./conseil.conf
-    sed -i "s/databaseName =*./$line1/g" "$conseil_conf_file"
-    sed -i "s/user =*./$line2/g" "$conseil_conf_file"
-    sed -i "s/password =*./$line3/g" "$conseil_conf_file"
+
+    sed -i "s/databaseName = ""conseil-local""/$line1/g" "$conseil_conf_file"
+    sed -i "s/user = ""conseiluser""*./$line2/g" "$conseil_conf_file"
+    sed -i "s/password = ""p@ssw0rd""/$line3/g" "$conseil_conf_file"
 
     cp "$PATH_TO_CONFIG"/conseil/runconseil-lorre.sh ./build/
     cp ./conseil.conf ./build/
