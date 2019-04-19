4f861f620d375c17d9473bbce76c5b63bd8a327b
leenpaws <s.chhaya@gmail.com>
2019-04-19 01:13:02 -0400

    Update build_conseil.sh

--- docker/app/conseil/build_conseil.sh
+++ docker/app/conseil/build_conseil.sh
@@ -61,7 +61,7 @@ build_conseil () {
 
     docker build -f "$DIR"/dockerfile -t conseil-"$DEPLOYMENT_ENV" .
     (( $? == 0 )) || fatal "Unable to build conseil container"
-   	docker run --name=conseil-"$DEPLOYMENT_ENV" --network=nautilus -d -p 1337:1337 5000:5000 conseil-"$DEPLOYMENT_ENV"
+   	docker run --name=conseil-"$DEPLOYMENT_ENV" --network=nautilus -d -p 1337:1337 -p 5000:5000 conseil-"$DEPLOYMENT_ENV"
     (( $? == 0 )) || fatal "Unable to run conseil container"
 	yes | docker system prune
 }
