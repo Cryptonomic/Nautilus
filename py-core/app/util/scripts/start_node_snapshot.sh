af11ed8fb5fd12859966196117388318f5ef24e4
PrathamRawat <prathamrawat815@gmail.com>
2020-07-20 12:56:03 -0400

    Add disclaimer, and remove RPC querying link

--- py-core/app/util/scripts/start_node_snapshot.sh
+++ py-core/app/util/scripts/start_node_snapshot.sh
@@ -16,7 +16,7 @@ cd "$4"
 
 if [ "$1" == "mainnet" ]; then
     if [ "$5" == "full" -o "$5" == "archive" ]; then
-        wget https://snaps.tulip.tools/mainnet_2020-07-13_16:00.full -O img
+        wget https://conseil-snapshots.s3.amazonaws.com/tezos-node_snapshot-latest.full -O img
     fi
     if [ "$5" == "experimental-rolling" ]; then
         wget https://snaps.tulip.tools/mainnet_2020-07-13_16:00.rolling -O img
@@ -30,10 +30,12 @@ else
     fi
 fi
 
-#docker run --rm -v "$PWD:/var/run/tezos/node/data" -v "$PWD:/config" "tezos/tezos:latest-release" tezos-config --network "$1" --cors-header='content-type' --cors-origin='*' --rpc-addr 127.0.0.1:"$2" --net-addr 127.0.0.1:"$3" --history-mode "$5" --config-file "/config/config.json" init
+if [ "$5" == "archive" ]; then
+    docker run --rm -v "$PWD:/var/run/tezos/" -v "$PWD/img:/snapshot" "tezos/tezos:latest-release" tezos-snapshot-import --reconstruct
+else
+    docker run --rm -v "$PWD:/var/run/tezos/" -v "$PWD/img:/snapshot" "tezos/tezos:latest-release" tezos-snapshot-import
+fi
 
-docker run --rm -v "$PWD:/var/run/tezos/" -v "$PWD/img:/snapshot" "tezos/tezos:latest-release" tezos-snapshot-import # --data-dir "/var/run/tezos/node/data" #--config-file "/config/config.json"
-
-docker run -d -v "$PWD:/var/run/tezos/" --network="host" --name "tezos-node-$4" "tezos/tezos:latest-release" tezos-node --network "$1" --cors-header='content-type' --cors-origin='*' --rpc-addr 127.0.0.1:"$2" --net-addr 127.0.0.1:"$3" --history-mode "$5" #--data-dir "/var/run/tezos/node/data" # --config-file "/config/config.json"
+docker run -d -v "$PWD:/var/run/tezos/" --network="host" --name "tezos-node-$4" "tezos/tezos:latest-release" tezos-node --network "$1" --cors-header='content-type' --cors-origin='*' --rpc-addr 127.0.0.1:"$2" --net-addr 127.0.0.1:"$3" --history-mode "$5"
 
 rm img
