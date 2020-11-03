c560eb559041368464d8f6ac25c4fafa6b77a211
PrathamRawat <prathamrawat815@gmail.com>
2020-11-03 15:54:54 -0500

    Split up node creation into functions, and create a new function for removing unneeded apps

--- py-core/app/util/tezos_node_functions.py
+++ py-core/app/util/tezos_node_functions.py
@@ -19,10 +19,13 @@ MAINNET_FULL_SNAPSHOT = "https://conseil-snapshots.s3.amazonaws.com/tezos-node_s
 MAINNET_ROLLING_SNAPSHOT = "https://conseil-snapshots.s3.amazonaws.com/tezos-node_snapshot-latest.full"
 CARTHAGENET_FULL_SNAPSHOT = "https://conseil-snapshots.s3.amazonaws.com/tezos-node_snapshot-latest.full"
 CARTHAGENET_ROLLING_SNAPSHOT = "https://conseil-snapshots.s3.amazonaws.com/tezos-node_snapshot-latest.full"
+DELPHINET_FULL_SNAPSHOT = "https://conseil-snapshots.s3.amazonaws.com/tezos-node_snapshot-latest.full"
+DELPHINET_ROLLING_SNAPSHOT = "https://conseil-snapshots.s3.amazonaws.com/tezos-node_snapshot-latest.full"
 
 # Data-Directory Download URLs
 MAINNET_DATA_DIR = "https://conseil-snapshots.s3.amazonaws.com/tezos-node_data-dir.tar.gz"
 CARTHAGENET_DATA_DIR = "https://conseil-snapshots.s3.amazonaws.com/tezos-data.tar.gz"
+DELPHINET_DATA_DIR = ""
 
 logging.basicConfig(filename=LOGGING_FILE_PATH,
                     format=LOGGING_FORMAT,
@@ -36,99 +39,12 @@ def create_node(data):
 
     # Start Node
     if data["restore"]:
-        shutil.copytree(DOCKER_COMPOSE_FILE_PATH + "reference-snapshot", DOCKER_COMPOSE_FILE_PATH + data["name"])
+        filename = download_node_snapshot(data)
+        load_snapshot_data(data, filename)
 
-        if data["network"] == "mainnet" and data["history_mode"] == "archive":
-            filename = wget.download(MAINNET_DATA_DIR, data_location)
+    create_new_node_directory(data)
 
-        if data["network"] == "mainnet" and data["history_mode"] == "full":
-            filename = wget.download(MAINNET_FULL_SNAPSHOT, data_location)
-
-        if data["network"] == "mainnet" and data["history_mode"] == "rolling":
-            filename = wget.download(MAINNET_ROLLING_SNAPSHOT, data_location)
-
-        if data["network"] == "carthagenet" and data["history_mode"] == "archive":
-            filename = wget.download(CARTHAGENET_DATA_DIR, data_location)
-
-        if data["network"] == "carthagenet" and data["history_mode"] == "full":
-            filename = wget.download(CARTHAGENET_FULL_SNAPSHOT, data_location)
-
-        if data["network"] == "carthagenet" and data["history_mode"] == "rolling":
-            filename = wget.download(CARTHAGENET_ROLLING_SNAPSHOT, data_location)
-
-        if data["history_mode"] == "archive":
-            file = tarfile.open(filename)
-            file.extractall(data_location)
-            file.close()
-            os.remove(filename)
-            # os.remove(data_location + "/tezos-data/node/data/identity.json")
-        else:
-            docker_volumes = dict()
-            docker_data_path = os.getcwd() + "/" + DOCKER_COMPOSE_FILE_PATH + data["name"] + "/tezos-data"
-            docker_snapshot_path = os.getcwd() + "/" + filename
-
-            docker_volumes[docker_data_path] = dict()
-            docker_volumes[docker_data_path]["bind"] = "/var/run/tezos"
-            docker_volumes[docker_data_path]["mode"] = "rw"
-
-            docker_volumes[docker_snapshot_path] = dict()
-            docker_volumes[docker_snapshot_path]["bind"] = "/snapshot"
-            docker_volumes[docker_snapshot_path]["mode"] = "rw"
-
-            docker_client = docker.from_env()
-            docker_client.containers.run("tezos/tezos:latest-release",
-                                         "tezos-snapshot-import",
-                                         auto_remove=True,
-                                         volumes=docker_volumes
-                                         )
-
-    elif data["network"] == "dalphanet":
-        shutil.copytree(DOCKER_COMPOSE_FILE_PATH + "reference-dalpha", DOCKER_COMPOSE_FILE_PATH + data["name"])
-    else:
-        shutil.copytree(DOCKER_COMPOSE_FILE_PATH + "reference", DOCKER_COMPOSE_FILE_PATH + data["name"])
-
-    file = open(DOCKER_COMPOSE_FILE_PATH + data["name"] + "/docker-compose.yml", "r")
-    text = file.read()
-    file.close()
-
-    text = text.replace("\"NODE START COMMAND\"",
-                        "\"tezos-node --cors-header='content-type' --cors-origin='*' --history-mode {} --network {} --rpc-addr 0.0.0.0:8732\""
-                        .format(
-                            data["history_mode"],
-                            data["network"]
-                            )
-                        )
-
-    logging.error(text)
-
-    if data["network"] != "dalphanet":
-        text = text.replace("\"TEZOS NETWORK\"",
-                            "\"{}\"".format(data["network"])
-                            )
-        text = text.replace("image: arronax",
-                            "image: arronax-{}".format(data["network"])
-                            )
-        text = text.replace("\"3080:80\"",
-                            "\"{}:80\"".format(data["arronax_port"])
-                            )
-        text = text.replace("\"4080:80\"",
-                            "\"{}:80\"".format(data["conseil_port"])
-                            )
-
-    text = text.replace("\"8732:8732\"",
-                        "\"{}:8732\"".format(data["node_port"])
-                        )
-
-    if data["history_mode"] == "archive":
-        text = text.replace("./tezos-data:/var/run/tezos",
-                            "./{}:/var/run/tezos".format("tezos-node_data-dir")
-                            )
-
-    logging.error(text)
-
-    file = open(DOCKER_COMPOSE_FILE_PATH + data["name"] + "/docker-compose.yml", "w")
-    file.write(text)
-    file.close()
+    parse_node_docker_compose_file(data)
 
     os.system(SCRIPT_FILE_PATH +
               "start_node.sh" +
@@ -137,6 +53,198 @@ def create_node(data):
               )
 
     update_status(data["name"], "bootstrapping")
+
+
+def download_node_snapshot(data):
+    data_location = DOCKER_COMPOSE_FILE_PATH + data["name"]
+    filename = ""
+
+    shutil.copytree(DOCKER_COMPOSE_FILE_PATH + "reference-snapshot", DOCKER_COMPOSE_FILE_PATH + data["name"])
+
+    if data["network"] == "mainnet" and data["history_mode"] == "archive":
+        filename = wget.download(MAINNET_DATA_DIR, data_location)
+
+    if data["network"] == "mainnet" and data["history_mode"] == "full":
+        filename = wget.download(MAINNET_FULL_SNAPSHOT, data_location)
+
+    if data["network"] == "mainnet" and data["history_mode"] == "rolling":
+        filename = wget.download(MAINNET_ROLLING_SNAPSHOT, data_location)
+
+    if data["network"] == "carthagenet" and data["history_mode"] == "archive":
+        filename = wget.download(CARTHAGENET_DATA_DIR, data_location)
+
+    if data["network"] == "carthagenet" and data["history_mode"] == "full":
+        filename = wget.download(CARTHAGENET_FULL_SNAPSHOT, data_location)
+
+    if data["network"] == "carthagenet" and data["history_mode"] == "rolling":
+        filename = wget.download(CARTHAGENET_ROLLING_SNAPSHOT, data_location)
+
+    if data["network"] == "delphinet" and data["history_mode"] == "archive":
+        filename = wget.download(DELPHINET_DATA_DIR, data_location)
+
+    if data["network"] == "delphinet" and data["history_mode"] == "full":
+        filename = wget.download(DELPHINET_FULL_SNAPSHOT, data_location)
+
+    if data["network"] == "delphinet" and data["history_mode"] == "rolling":
+        filename = wget.download(DELPHINET_ROLLING_SNAPSHOT, data_location)
+
+    return filename
+
+
+def load_snapshot_data(data, filename):
+    data_location = DOCKER_COMPOSE_FILE_PATH + data["name"]
+
+    if data["history_mode"] == "archive":
+        file = tarfile.open(filename)
+        file.extractall(data_location)
+        file.close()
+        os.remove(filename)
+        # os.remove(data_location + "/tezos-data/node/data/identity.json")
+    else:
+        docker_volumes = dict()
+        docker_data_path = os.getcwd() + "/" + DOCKER_COMPOSE_FILE_PATH + data["name"] + "/tezos-data"
+        docker_snapshot_path = os.getcwd() + "/" + filename
+
+        docker_volumes[docker_data_path] = dict()
+        docker_volumes[docker_data_path]["bind"] = "/var/run/tezos"
+        docker_volumes[docker_data_path]["mode"] = "rw"
+
+        docker_volumes[docker_snapshot_path] = dict()
+        docker_volumes[docker_snapshot_path]["bind"] = "/snapshot"
+        docker_volumes[docker_snapshot_path]["mode"] = "rw"
+
+        docker_client = docker.from_env()
+        docker_client.containers.run("tezos/tezos:latest-release",
+                                     "tezos-snapshot-import",
+                                     auto_remove=True,
+                                     volumes=docker_volumes
+                                     )
+
+
+def create_new_node_directory(data):
+    if data["network"] == "dalphanet":
+        shutil.copytree(DOCKER_COMPOSE_FILE_PATH + "reference-dalpha", DOCKER_COMPOSE_FILE_PATH + data["name"])
+    else:
+        shutil.copytree(DOCKER_COMPOSE_FILE_PATH + "reference", DOCKER_COMPOSE_FILE_PATH + data["name"])
+
+
+def remove_node_options(data, file_text):
+    if data["network"] != "dalphanet":
+        if data["arronax_port"] == 0:
+            file_text = file_text.replace(
+                """
+arronax:
+  image: arronax
+  restart: always
+  ports:
+    - "3080:80"
+  networks:
+    - tezos
+  logging:
+    driver: "json-file" 
+                """, "")
+        if data["conseil_port"] == 0:
+            file_text = file_text.replace(
+                """
+conseil-postgres:
+  image: postgres:11.6
+  restart: always
+  environment:
+    POSTRGES_PASSWORD: "password"
+    POSTGRES_USER: "conseil"
+    POSTGRES_DB: "conseil"
+    POSTGRES_INITDB_ARGS: "--lc-collate=en_US.UTF-8 -E UTF8"
+  expose:
+    - 5423
+  networks:
+    - tezos
+  volumes:
+    - ../../data/conseil.sql:/docker-entrypoint-initdb.d/conseil.sql
+  logging:
+    driver: "json-file"
+
+conseil-lorre:
+  image: cryptonomictech/conseil:latest
+  restart: always
+  environment:
+    DB_Host: "conseil-postgres"
+    DB_Port : 5432
+    DB_User: "conseil"
+    DB_Password: "password"
+    DB_Database: "conseil"
+    XTZ_Host: "tezos-node"
+    XTZ_Port: 8732
+    XTZ_Network": "TEZOS NETWORK"
+  command: "conseil-lorre"
+  networks:
+    - tezos
+  logging:
+    driver: "json-file"
+
+conseil-api:
+  image: cryptonomictech/conseil:latest
+  restart: always
+  environment:
+    DB_Host: "conseil-postgres"
+    DB_Port: 5432
+    DB_User: "conseil"
+    DB_Password: "password"
+    DB_Database: "conseil"
+    XTZ_Host: "tezos-node"
+    XTZ_Port: 8732
+    XTZ_Network: "TEZOS NETWORK"
+  command: "conseil-api"
+  ports:
+    - "4080:80"
+  expose:
+    - 80
+  networks:
+    - tezos
+  logging:
+    driver: "json-file"
+            """, " ")
+    return file_text
+
+
+def parse_node_docker_compose_file(data):
+    file = open(DOCKER_COMPOSE_FILE_PATH + data["name"] + "/docker-compose.yml", "r")
+    node_docker_compose_text_file = file.read()
+    file.close()
+
+    node_docker_compose_text_file = node_docker_compose_text_file.replace("\"NODE START COMMAND\"",
+                        "\"tezos-node --cors-header='content-type' --cors-origin='*' --history-mode {} --network {} --rpc-addr 0.0.0.0:8732\""
+                        .format(
+                            data["history_mode"],
+                            data["network"]
+                        )
+                        )
+
+    if data["network"] != "dalphanet":
+        node_docker_compose_text_file = node_docker_compose_text_file.replace("\"TEZOS NETWORK\"",
+                            "\"{}\"".format(data["network"])
+                            )
+        node_docker_compose_text_file = node_docker_compose_text_file.replace("image: arronax",
+                            "image: arronax-{}".format(data["network"])
+                            )
+        node_docker_compose_text_file = node_docker_compose_text_file.replace("\"3080:80\"",
+                            "\"{}:80\"".format(data["arronax_port"])
+                            )
+        node_docker_compose_text_file = node_docker_compose_text_file.replace("\"4080:80\"",
+                            "\"{}:80\"".format(data["conseil_port"])
+                            )
+
+    node_docker_compose_text_file = node_docker_compose_text_file.replace("\"8732:8732\"",
+                        "\"{}:8732\"".format(data["node_port"])
+                        )
+
+    if data["history_mode"] == "archive":
+        node_docker_compose_text_file = node_docker_compose_text_file.replace("./tezos-data:/var/run/tezos",
+                            "./{}:/var/run/tezos".format("tezos-node_data-dir")
+                            )
+
+    file = open(DOCKER_COMPOSE_FILE_PATH + data["name"] + "/docker-compose.yml", "w")
+    file.write(node_docker_compose_text_file)
+    file.close()
 
 
 def stop_node(name):
