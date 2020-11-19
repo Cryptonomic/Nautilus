3a13f71881757fb1502a579720aa56531e51d5f9
PrathamRawat <prathamrawat815@gmail.com>
2020-11-19 18:54:00 -0500

    Add compatibility for the new ebetanet

--- py-core/setup_workspace.sh
+++ py-core/setup_workspace.sh
@@ -5,6 +5,8 @@ mkdir ~/.nautilus-core
 # Pull Tezos Docker Image
 docker pull tezos/tezos:latest-release
 
+docker pull tezos/tezos:ebetanet-release
+
 # Pull Redis Docker Container for Python Job Queue
 docker pull redis
 
@@ -98,6 +100,38 @@ export default configs;
 " >> src/config.tsx
 
 docker build -t arronax-delphinet .
+
+# shellcheck disable=SC2103
+cd ..
+
+rm -rf Arronax
+
+#Build Ebetanet Arronax Image Locally
+git clone https://github.com/Cryptonomic/Arronax.git
+
+cd Arronax
+
+touch src/config.tsx
+
+echo "import { Config } from './types';
+
+const configs: Config[] = [
+  {
+    platform: 'tezos',
+    network: 'mainnet',
+    displayName: 'Tezos Mainnet',
+    url: 'http://conseil-api:80',
+    apiKey: 'conseil',
+    nodeUrl: 'http://tezos-node:8732',
+    entities: ['blocks', 'operations', 'accounts', 'bakers', 'governance'],
+    hiddenEntities: ['originated_account_maps', 'big_maps', 'big_map_contents']
+  }
+]
+
+export default configs;
+" >> src/config.tsx
+
+docker build -t arronax-ebetanet .
 
 # shellcheck disable=SC2103
 cd ..
